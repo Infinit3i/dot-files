71c3c2446bcf03df98bdca382332863f03697614
infinit3i <matthewiver@protonmail.com>
2026-02-23 14:37:04 -0500

    fix weather so it shows hot or cold

---
+++ waybar/.config/waybar/scripts/weather.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+weather=$(curl -s 'https://wttr.in/?format=%t+%C&u')
+temp=$(echo "$weather" | grep -oE '[-]?[0-9]+' | head -1)
+
+if [ "$temp" -lt 45 ]; then
+    echo "{\"text\":\"$weather\",\"class\":\"cold\"}"
+elif [ "$temp" -gt 75 ]; then
+    echo "{\"text\":\"$weather\",\"class\":\"hot\"}"
+else
+    echo "{\"text\":\"$weather\"}"
+fi
