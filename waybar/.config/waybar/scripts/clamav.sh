d5babab8bcbe70c9fb2ccb79689bec9aedacb11f
infinit3i <matthewiver@protonmail.com>
2026-03-31 11:50:19 -0400

    update clamav

--- waybar/.config/waybar/scripts/clamav.sh
+++ waybar/.config/waybar/scripts/clamav.sh
@@ -3,14 +3,16 @@
 LOGFILE="$HOME/.local/share/clamav-hourly.log"
 
 if [[ ! -f "$LOGFILE" ]]; then
-    echo "ClamAV: N/A"
+    printf '{"text":"N/A","tooltip":"ClamAV","class":"normal"}\n'
     exit 0
 fi
 
 INFECTED=$(grep -i "Infected files" "$LOGFILE" | tail -1 | awk '{print $3}')
 
-if [[ "$INFECTED" =~ ^[0-9]+$ ]]; then
-    echo "ClamAV: $INFECTED"
+if [[ ! "$INFECTED" =~ ^[0-9]+$ ]]; then
+    printf '{"text":"N/A","tooltip":"ClamAV","class":"normal"}\n'
+elif [[ "$INFECTED" -gt 0 ]]; then
+    printf '{"text":"ClamAV: %s","tooltip":"ClamAV","class":"critical"}\n' "$INFECTED"
 else
-    echo "ClamAV: N/A"
+    printf '{"text":"0","tooltip":"ClamAV","class":"normal"}\n'
 fi
