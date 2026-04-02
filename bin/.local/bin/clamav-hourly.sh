1d82a958d7e27ccc03aa4b0cfe7448a09dfe110e
infinit3i <matthewiver@protonmail.com>
2026-04-02 13:43:27 -0400

    update clamav

--- bin/.local/bin/clamav-hourly.sh
+++ bin/.local/bin/clamav-hourly.sh
@@ -7,4 +7,16 @@ clamdscan \
   --multiscan \
   --fdpass \
   --infected \
-  "$SCAN_DIR" >> "$LOGFILE"
+  --exclude-dir='^/home/infinit3i/\.cache' \
+  --exclude-dir='^/home/infinit3i/\.local/share/docker' \
+  --exclude-dir='^/home/infinit3i/\.local/share/containers' \
+  --exclude-dir='^/home/infinit3i/\.local/share/Trash' \
+  "$SCAN_DIR" >> "$LOGFILE" 2>&1
+
+# clamdscan exits 1 for infections found, 2 for errors
+# Only fail on actual errors, not on "infected found"
+exit_code=$?
+if [ "$exit_code" -eq 2 ]; then
+  exit 2
+fi
+exit 0
