3e9eb44731f4cc674831fc3d37a33c7c14a6f04b
infinit3i <matthewiver@protonmail.com>
2026-04-06 21:03:42 -0400

    update hyprsunset and clamav

--- bin/.local/bin/clamav-hourly.sh
+++ bin/.local/bin/clamav-hourly.sh
@@ -3,15 +3,24 @@
 LOGFILE="$HOME/.local/share/clamav-hourly.log"
 SCAN_DIR="$HOME"
 
-clamdscan \
-  --multiscan \
-  --fdpass \
-  --infected \
-  --exclude-dir='^/home/infinit3i/\.cache' \
-  --exclude-dir='^/home/infinit3i/\.local/share/docker' \
-  --exclude-dir='^/home/infinit3i/\.local/share/containers' \
-  --exclude-dir='^/home/infinit3i/\.local/share/Trash' \
-  "$SCAN_DIR" >> "$LOGFILE" 2>&1
+# clamdscan does not honor --exclude-dir, so we build the file list via
+# `find` with -prune and feed it through --file-list.
+find "$SCAN_DIR" \
+  \( \
+       -path "$HOME/.cache" \
+    -o -path "$HOME/.local/share/docker" \
+    -o -path "$HOME/.local/share/containers" \
+    -o -path "$HOME/.local/share/Trash" \
+    -o -path "$HOME/.zoom" \
+    -o -path "$HOME/.config/zoom" \
+    -o -path "$HOME/.steam" \
+    -o -path "$HOME/.config/obsidian/IndexedDB" \
+  \) -prune -o -type f -print 2>/dev/null \
+  | clamdscan \
+      --multiscan \
+      --fdpass \
+      --infected \
+      --file-list=- >> "$LOGFILE" 2>&1
 
 # clamdscan exits 1 for infections found, 2 for errors
 # Only fail on actual errors, not on "infected found"
